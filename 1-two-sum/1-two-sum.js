e208707073a7c507f54861255704b3b43987934e
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-07-17 21:32:42 -0600

    Time: 76 ms (90.13%), Space: 42.7 MB (51.46%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,11 +5,20 @@
  */
 var twoSum = function(nums, target) {
     // if (nums.length < 2) return
-    for (var i = 0; i < nums.length - 1; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            if ((nums[i] + nums[j]) === target) { return  [i, j]  }
-        } 
+    let hash = {}
+    for (var i = 0; i < nums.length; i++) {
+        if (target - nums[i] in hash) return [hash[ target - nums[i]], i]
+        else { hash[nums[i]] = i }
     }
+    
+//         let map = {};
+//         for (let i = 0; i < nums.length; i++) {
+//         if (target - nums[i] in map) {
+//             return [map[target-nums[i]], i];
+//         } else {
+//             map[nums[i]] = i;
+//         }
+//     }
 };
     
     
@@ -17,11 +26,8 @@ var twoSum = function(nums, target) {
 iterate over the array 
 
 for every element in the array 
- 
- iterate over the array again
 
-subtract the amount from the total
-add the numbers indice to an array
+subtract the amount
 
 if the amount subtracted is greater than are target
 */
