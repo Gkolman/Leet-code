fdf4f3e5ee1385569eadb6fc9593cffdf55d0f27
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-07-19 21:17:12 -0600

    Time: 168 ms (74.92%), Space: 53.6 MB (49.08%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -4,26 +4,20 @@
  */
 
 var groupAnagrams = function(strs) {
-    // iterate over the array 
     let anagrams = {}
     for (var i = 0; i < strs.length; i++) {
         let word = strs[i]
         let sorted = word.split('').sort((a, b) => {
-                                            if (a < b) return -1;
-                                            if ( a > b) return 1;
-                                            return 0
-                                        }).join('')
-        if ( sorted in anagrams ) { anagrams[sorted].push(i)}
-        else { anagrams[sorted] = [i]}
+            if (a < b) return -1;
+            if ( a > b) return 1;
+            return 0
+            }).join('')
+        if ( sorted in anagrams ) { anagrams[sorted].push(strs[i])}
+        else { anagrams[sorted] = [strs[i]] }
     }
     let result = []
     for (var word in anagrams) {
-        let words = []
-        let indices = anagrams[word]
-        indices.forEach((indice) =>{
-            words.push(strs[indice])
-        })
-        result.push(words)
+        result.push(anagrams[word])
     }
     return result
 }
