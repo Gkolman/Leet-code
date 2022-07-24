a0b841d2ff5c1fdd0eba19b17514dc9b49e59264
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-07-24 14:54:44 -0600

    Time: 91 ms (82.55%), Space: 44.2 MB (98.09%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -2,21 +2,6 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-
-
-
-// let isUniqueSmall = (row, col, board) => {
-//     let unique = {}
-//     for (var left = 0; left <  board.length; left++) {
-//         for (var)
-//         let num = board[row][left]
-//         if (num == '.') {continue}
-//         if (num in unique) {return false}
-//         else {unique[num] = 1}
-//     }
-//     return true
-// } 
-
 let isUniqueBig = (pointer, board, type) => {
     let unique = {}
     for (var i = 0; i < board.length; i++) {
@@ -32,17 +17,11 @@ let isUniqueBig = (pointer, board, type) => {
 
 let isUniqueSmall = (r, c, board) => {
     let unique = {}
-    console.log('r, c -> ', r, c)   
     for (var row = r; row < r + 3; row++) {
         for (var col = c; col < c + 3; col++) {
             let num = board[row][col]
             if (num == '.') { continue }
-            if (num in unique) {
-                // console.log('row, col -> ', row, col)   
-                // console.log('unique -> ', unique)   
-                // console.log('num -> ', num)   
-                return false
-            }
+            if (num in unique) {return false}
             else {unique[num] = 1}
         }
     }
@@ -58,22 +37,9 @@ var isValidSudoku = function(board) {
     
     for (let r = 0;  r < board.length; r += 3) {
         for (let c = 0;  c < board.length; c += 3) {
-        // console.log('r, c -> ', r, c)
-        // console.log('passes  -> ',  isUniqueSmall(r, c, board) )
             if (!isUniqueSmall(r, c, board)) { return false}
         }
     }
     
     return true
-    
-    //
-    
-    
-    // [1,3,4] 
-    // [5,6,7]
-    // [8,9,2] true 
-    // 
-    
-    // iterate diagnonally
-    
 };
