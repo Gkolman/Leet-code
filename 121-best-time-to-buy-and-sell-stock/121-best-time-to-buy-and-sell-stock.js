5e745db1695e843a15e9ae9336ab942b8b9c1909
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-08-25 15:26:20 -0600

    Time: 147 ms (25.28%), Space: 51.4 MB (91.74%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,27 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
-    // iterate over the array
-    // we have one pointer leftthat marks a buy time 
-    // while the right arrow 
-    // while the right number is smaller than the left number 
-    
-    // [7,1,5,3,6,4]
-    //      ^ ^    
-    // if right < left increament right pointr 
-    // 
     let largest = 0
     let l = 0, r = 1
     while (r < prices.length) {
-        // each pointer must have a plus one and a minus one
         if (prices[r] <= prices[l])  { l = r } 
         largest = Math.max(largest,  prices[r] - prices[l])
         r+=1 
-        // else { l = r; r = r+1}
-        // while (prices[r + 1] === prices[r]) { r+=1  }
-        // while ( prices[l + 1]  === prices[l]) { l+=1}
-        // else{ l += 1; r +=1}
     }
         
     return largest
