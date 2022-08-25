ae5375e0323bacb8afaf7200a0484eaf36357836
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-08-25 15:23:10 -0600

    Time: 153 ms (5.16%), Space: 45 MB (5.80%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -1,40 +1,16 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var mergeTwoLists = function(l1, l2) {
-    
-    let newList;
-    let pointer1 = l1
-    let pointer2 = l2
-    let pointer3 = newList    
-    if (!l1) { return l2}
-    if (!l2) { return l1}
-    while (pointer1) {
-        if (!pointer2) break;
-        if (pointer1.val <= pointer2.val) {
-            if (!newList) {newList = new ListNode(pointer1.val); pointer3 = newList}
-            else {pointer3.next = new ListNode(pointer1.val); pointer3 = pointer3.next}
-            pointer1 = pointer1.next
-        } else {
-            if (!newList) { newList = new ListNode(pointer2.val); pointer3 = newList}
-            else {pointer3.next = new ListNode(pointer2.val); pointer3 = pointer3.next}
-            pointer2 = pointer2.next  
-        }  
-    }
-    while (pointer2) {
-        pointer3.next = new ListNode(pointer2.val); pointer3 = pointer3.next;pointer2 = pointer2.next  
-    }
-    while (pointer1) {
-        pointer3.next = new ListNode(pointer1.val); pointer3 = pointer3.next;pointer1 = pointer1.next  
+var mergeTwoLists = function(list1, list2) {
+    let storage = []
+    let current = list1
+    while (current) { storage.push(current.val); current = current.next}
+    current = list2
+    while (current) { storage.push(current.val); current = current.next}
+    let sorted = storage.sort((a,b) => {return a - b})
+    // console.log(sorted)
+    let newList = sorted.length > 0 ? new ListNode(sorted[0]) : null
+    for (var i = 1; i < sorted.length; i++){
+        let current = newList
+        while (current.next) { current = current.next}
+        if (current) { current.next = new ListNode(sorted[i]) } 
     }
     return newList
 };
