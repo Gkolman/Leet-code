49935b245a0289b0a2619c432f75a31f71c48d3d
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-08-29 15:46:51 -0600

    Time: 2846 ms (5.03%), Space: 49.1 MB (5.48%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.js
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -4,15 +4,17 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    let result = new Set()
+    let result = {}
     let nums = {}
     for (var c of candidates) {
         if (c in nums) { nums[c] +=1} else {nums[c] = 1}
     }
+    
     let traverse = (target, storage, path = []) => {
         if (target === 0) {
             let key = path.slice().sort((a,b) => {return a - b}).join(',')  
             result[key] = true
+            return 
         }
         if (target < 0) return;
         for (var key in storage) {
