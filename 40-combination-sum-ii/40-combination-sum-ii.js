9579fa03a0383eb1d35011ec03d384f89d66a2a5
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-08-29 15:23:54 -0600

    Time: 3158 ms (5.03%), Space: 48.7 MB (7.29%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    let result = new Set()
+    let nums = {}
+    for (var c of candidates) {
+        if (c in nums) { nums[c] +=1} else {nums[c] = 1}
+    }
+    let traverse = (target, storage, path = []) => {
+        if (target === 0) {
+            let key = path.slice().sort((a,b) => {return a - b}).join(',')  
+            result[key] = true
+        }
+        if (target < 0) return;
+        for (var key in storage) {
+            let copy = JSON.parse(JSON.stringify(storage))
+            if (copy[key] === 1) {delete copy[key]}
+            else {copy[key] -=1}
+            path.push(key)
+            traverse(target - key, copy, path)
+            path.pop()
+        }
+    }
+    traverse(target, nums)
+    return Object.keys(result).map((cv) => {return cv.split(',')})    
+};
