37014d627860a8404386d72dc7e36b80a49d4f03
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-09-01 14:02:35 -0600

    Time: 81 ms (64.66%), Space: 42.5 MB (27.65%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -10,10 +10,9 @@ var searchInsert = function(nums, target) {
     while (left <= right ) {
         let mid = Math.floor((left + right) / 2) //0
         if (nums[mid] === target) { return mid }
-    
         if (target < nums[mid] ){
             right = mid - 1
-        } else if(target > nums[mid]) {left = mid + 1}
+        } else {left = mid + 1}
         
     }
     return left
