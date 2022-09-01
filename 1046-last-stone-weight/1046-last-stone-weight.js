02926806e0a18a5d2202cac7e64f839db6a7294b
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-09-01 14:05:10 -0600

    Time: 200 ms (5.24%), Space: 47.4 MB (5.24%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -2,32 +2,38 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function(stones) {
 
-    let changed = true    
-    while(stones.length > 1) {
-     if (changed) { stones.sort((a,b) => {return a -b}) }
-     let sum = stones[stones.length - 1]  - stones[stones.length - 2]
-     if (sum > 0) {
-         stones.splice(stones.length-2, 2, sum)
-         changed = true
-     } else {
-         stones.splice(stones.length -2, 2)
-         changed = false
-     }
-    }    
-    return !stones.length ? 0 : stones[0]
-    
-    /*
-     iterate over the array 
-     store the two largest numbers and there indices
-     smash them -> remainder if remainedr is 0 splice out both elements 
-     if there is a remainder splice out the left element and susbtitute the right element with the remainder
-        [2,7,4,1,8,1]
-        [1,1,2,4,7,8]
-        [1,1,2,4,1]
-        [1,1,2,1]
-        [1,1,1]
-        [1]
-    */
+let structure = function(elements) {
+    this.storage = elements.sort((a,b) => { return a - b })
+    this.add = (number) => {
+        if (this.storage.length === 0) {this.storage.push(number); return}
+        let left = 0
+        let right = this.storage.length - 1
+        while (left <= right ) {
+            let mid = Math.floor((left + right) / 2) //0
+            let sum = right - mid - left // 0
+            if (number === this.storage[mid]) {
+                this.storage.splice(mid, 0, number); return
+            } else if (number > this.storage[mid] ){
+                left = mid + 1
+            } else {right = mid - 1}
+        }
+        this.storage.splice(left, 0, number)
+    }
+}
+var lastStoneWeight = function(stones) {
+    let storage = new structure(stones.slice())
+    while(storage.storage.length > 1) {
+        let sum = storage.storage[storage.storage.length - 1]  - storage.storage[storage.storage.length - 2]
+        if (sum > 0) {
+            console.log('before', storage.storage)
+            storage.storage.splice(storage.storage.length-2, 2)
+            storage.add(sum)
+            console.log('after', storage.storage)
+         } else {
+             storage.storage.splice(storage.storage.length -2, 2)
+         }
+    }
+    // console.log(storage.storage)
+    return !storage.storage.length ? 0 : storage.storage[0]
 };
