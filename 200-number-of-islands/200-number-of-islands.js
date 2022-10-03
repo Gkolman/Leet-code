9a4367a2377684efbabf83f895cb43ee0ac6a18f
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2022-10-03 15:23:33 -0700

    Time: 93 ms (89.22%), Space: 44.6 MB (86.50%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let islands = 0
+    const recurse = (row, col) => {
+        grid[row][col] = '2'
+        if (grid[row][col-1] ==='1'){recurse(row, col-1)}  // left
+        if (grid[row][col+1] ==='1'){recurse(row, col+1)}  // right
+        if (grid[row-1]) {if (grid[row -1][col] ==='1'){recurse(row - 1, col)}} // up 
+        if (grid[row+1]) {if (grid[row+1][col] ==='1'){recurse(row+1, col)}} // down
+    }
+    for (var row = 0; row < grid.length; row+=1) {
+        for (var col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === '1') {recurse(row,col);islands+=1}
+        }
+    }
+    return islands
+};
