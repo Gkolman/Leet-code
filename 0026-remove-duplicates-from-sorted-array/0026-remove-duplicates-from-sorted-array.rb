ce2fd91c685b095fad66741e398ef6b9ec12ad29
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2023-02-05 21:07:46 -0800

    Time: 82 ms (94.80%), Space: 212.1 MB (10.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
@@ -1,17 +1,10 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def remove_duplicates(nums)
-    storage = Set.new
-    i = 0
-    while i < nums.length do
-        num = nums[i]
-        if storage.include?(num)
-            nums.slice!(i, 1)
-            i-=1
-        else
-            storage.add(num)
-        end
-        i+=1
+    if nums.uniq == nums
+        nums.length
+    else 
+        nums = nums.uniq!
+        nums.length
     end
-    nums.length
 end
