c11c07b976e6bd0582c959685efb13bccb642b50
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2023-02-07 09:28:27 -0800

    Time: 93 ms (28.23%), Space: 211 MB (78.23%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.rb
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q, valid = true)
+    if valid == false 
+        return valid
+    elsif p == nil && q == nil
+       return valid
+    elsif p && q == nil || p == nil && q
+        return valid = false
+    elsif p.val != q.val
+        return valid = false
+    end
+    if is_same_tree(p.left, q.left, valid) == false
+        false
+    elsif is_same_tree(p.right, q.right, valid) == false
+        false
+    else
+        true
+    end
+end
