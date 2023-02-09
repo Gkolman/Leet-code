00f5108d8d1f854da6078f62825719afba68baca
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2023-02-09 12:44:58 -0800

    Time: 131 ms (27.80%), Space: 57.1 MB (5.16%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    let result;
+    let minRange = 1000 ** 1000;
+    let results = []
+    nums = nums.sort((a,b) => {return a - b})
+    for (var left = 0; left < nums.length - 2; left++) {
+        let middle = left+1
+        let right = nums.length-1
+        while (middle < right) {            
+            let sum = nums[left] + nums[middle] + nums[right]
+            results.push(sum)
+            let newRange = Math.abs(sum - target)
+            if (newRange < minRange) {
+                result = sum
+                minRange = newRange
+            }
+            if (sum < target) {  
+                middle+=1
+            } else if (sum > target)  {
+                right-=1
+            } else if (sum === target){
+                return sum
+            }
+        }
+    } 
+    return result
+};
