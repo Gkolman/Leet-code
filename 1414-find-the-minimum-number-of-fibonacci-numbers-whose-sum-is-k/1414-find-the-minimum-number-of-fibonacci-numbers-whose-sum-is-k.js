896c3d4f0a1e2d8c7c14bb23398547f52867a044
Gage  Guzman <54620463+Gkolman@users.noreply.github.com>
2023-02-10 13:27:28 -0800

    Time: 145 ms (11.11%), Space: 52 MB (7.41%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.js
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.js
@@ -5,23 +5,16 @@
 var findMinFibonacciNumbers = function(k) {
     const cache = {1: 1, 2: 2, 3: 3}
     let reached = false
-    const fibonacci = (num) => {
-        if (cache[num]) { return cache[num] }
-        let leftTree = fibonacci(num-1)
-        if (reached) { return }
-        let rightTree = fibonacci(num-2)
-        const sum = leftTree + rightTree
-        cache[num] = sum
-        if (sum > k || reached) { 
-            reached = true
-            return cache[num]
-        } else { 
-            fibonacci(num+1) 
-        }
+    const values = [1,1]
+    let first = 1
+    let second = 1
+    while (values[values.length - 1] < k) {
+       const sum = first + second
+       first = second
+       second = sum
+       values.push(sum)
     }
-    fibonacci(4)
-    const values = Object.values(cache)
-    const traverse = (target, depth = 0, memo={}, index = values.length -1) => {
+    const traverse = (target, depth = 0, memo={}) => {
         if (memo[0]) { return memo }
         if (!memo[target]) {memo[target] = depth} 
         else {
@@ -29,8 +22,8 @@ var findMinFibonacciNumbers = function(k) {
             else { memo[target] = depth }
         }
         if (target <= 0 ) { return memo }
-        for (var i = index; i > -1; i--) {
-            traverse(target - values[i], depth+1, memo, index-1)
+        for (var i = values.length -1; i > -1; i--) {
+            traverse(target - values[i], depth+1, memo)
         }
         return memo
     }
